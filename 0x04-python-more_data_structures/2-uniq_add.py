a0febc0f887a832f76daf6d8a045e072f31e2e63
McMugah <blainelium@gmail.com>
2023-06-14 21:32:22 +0300

    add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+def uniq_add(my_list=[]):
+    return sum(set(my_list))
