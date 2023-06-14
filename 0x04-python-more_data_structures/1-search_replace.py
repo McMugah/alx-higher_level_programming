e5994c799b1e4b5d2569688179c445a6558b0416
McMugah <blainelium@gmail.com>
2023-06-14 21:34:36 +0300

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if 0 <= idx < len(my_list):
-        return my_list[idx]
-    return None
+def search_replace(my_list, search, replace):
+    return [replace if num == search else num for num in my_list]
